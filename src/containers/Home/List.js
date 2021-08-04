b8221329adab19312c5f2d3a36f84fe9177847f9
AmirChek7 <amirbekmamasaliev3@gmail.com>
2021-08-04 17:02:30 +0600

    first work

---
+++ src/containers/Home/List.js
@@ -0,0 +1,31 @@
+import React, { useContext, useEffect } from 'react';
+import { clientContext } from '../../contexts/ClientContext';
+import MediaCard from './MediaCard';
+
+const List = () => {
+    const { getProducts, products } = useContext(clientContext)
+
+    useEffect(() => {
+        // getProducts()
+    }, [])
+
+    return (
+        <div className="list">
+            {
+                products ? (
+                    products.length ? (
+                        products.map(product => (
+                            <MediaCard key={product.id} product={product} />
+                        ))
+                    ) : (
+                        <h1>Нету товаров</h1>
+                    )
+                ) : (
+                    <h1>Loading...</h1>
+                )
+            }
+        </div>
+    );
+};
+
+export default List;
